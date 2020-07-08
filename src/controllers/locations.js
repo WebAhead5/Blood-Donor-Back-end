e202919db6f349d2bf4e9912a3684f460b88b8db
fridabu <f0504070002@gmail.com>
2020-07-08 10:27:53 +0300

    debug

--- src/controllers/locations.js
+++ src/controllers/locations.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require("https");
-const {updateGeolocations} = require('./updateGeolocations')
+const { updateGeolocations } = require('./updateGeolocations')
+const axios = require('axios');
 
 
 exports.getAllLocations = (req, res) => {
@@ -23,7 +24,7 @@ exports.getAllLocations = (req, res) => {
 
 
 exports.getLocationsIframe = (req, res) => {
-    fetchLocations().then((result) => {
+    fetchLocations2().then((result) => {
         res.status(200).render('locations', {
             // encodeURIComponent so later we can use the data in the script tag as a javascript value
             result: encodeURIComponent(JSON.stringify(result)),
@@ -123,5 +124,102 @@ fetchLocations = () => new Promise(
             RequestData: ''
         }));
         req.end();
+    }
+);
+
+
+
+fetchLocations2 = () => new Promise(
+    (resolve, reject) => {
+        const bodyFormData = JSON.stringify({
+            RequestHeader:
+            {
+                Application: 101,
+                Module: 'BloodBank',
+                Function: 'GetAllDetailsDonations',
+                Token: ''
+            },
+            RequestData: ''
+        });
+
+        axios({
+            method: 'post',
+            url: 'https://www.mdais.org/umbraco/api/invoker/execute',
+            data: bodyFormData,
+            headers: {
+                "accept": "application/json, text/plain, */*",
+                "content-type": "application/json",
+                "cache-control": "no-cache",
+                "postman-token": "f6b4a7b0-2873-f2fe-18d7-241704b44b78",
+                "cookie": "GCLB=CJSY9sTAh7WIhQE; rbzsessionid=2a9b60db124b2002c01afb275fb854c0; _ga=GA1.2.1892371988.1592293688; _fbp=fb.1.1592293688005.696364955; __atssc=google%3B2; _gid=GA1.2.474835944.1592819432; rbzid=0TBhmOE820ArGr4ItiazDPOBPc9evHbIcro2K2EZ02Xx4X4v2FbHKXLuPQdi4q89XnGC/2mW3EWjYW16mSyXB+wilbnuqkDhkd/yiJGC5ZibiUR0FPnYFhI3u/wM95ggPbtpTU4KO6Fi9aVM2hVwB0+ZNokYKG3ibAlKljwK49N45kkXtCvaaGQcbBV8H1+S9fzlKt2m8Kyo5tSf9SWCoziWSbHYXFajWsygptQQ8HbdDjgm35VZ4db1SVrtfLdQ47OQqvZx0qiqKCvO8dXXEA==; __atuvc=25%7C25%2C6%7C26; __atuvs=5ef0a8db5474d3c4001",
+                "origin": "https://www.mdais.org",
+                "authority": "www.mdais.org",
+                "accept-encoding": "json",
+                "accept-language": "en-US,en;q=0.9,he-IL;q=0.8,he;q=0.7,ar-IL;q=0.6,ar;q=0.5",
+                "referer": "https://www.mdais.org/blood-donation",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"
+            }
+        })
+            .then(function (response) {
+                //handle success
+                console.log(response);
+                resolve(response)
+            })
+            .catch(function (response) {
+                //handle error
+                console.log(response);
+            });
+        //     const options = {
+        //         "method": "POST",
+        //         "hostname": "www.mdais.org",
+        //         "port": null,
+        //         "scheme": "https",
+        //         "path": "/umbraco/api/invoker/execute",
+        //         "headers": {
+        //             "accept": "application/json, text/plain, */*",
+        //             "content-type": "application/json",
+        //             "cache-control": "no-cache",
+        //             "postman-token": "f6b4a7b0-2873-f2fe-18d7-241704b44b78",
+        //             "cookie": "GCLB=CJSY9sTAh7WIhQE; rbzsessionid=2a9b60db124b2002c01afb275fb854c0; _ga=GA1.2.1892371988.1592293688; _fbp=fb.1.1592293688005.696364955; __atssc=google%3B2; _gid=GA1.2.474835944.1592819432; rbzid=0TBhmOE820ArGr4ItiazDPOBPc9evHbIcro2K2EZ02Xx4X4v2FbHKXLuPQdi4q89XnGC/2mW3EWjYW16mSyXB+wilbnuqkDhkd/yiJGC5ZibiUR0FPnYFhI3u/wM95ggPbtpTU4KO6Fi9aVM2hVwB0+ZNokYKG3ibAlKljwK49N45kkXtCvaaGQcbBV8H1+S9fzlKt2m8Kyo5tSf9SWCoziWSbHYXFajWsygptQQ8HbdDjgm35VZ4db1SVrtfLdQ47OQqvZx0qiqKCvO8dXXEA==; __atuvc=25%7C25%2C6%7C26; __atuvs=5ef0a8db5474d3c4001",
+        //             "origin": "https://www.mdais.org",
+        //             "authority": "www.mdais.org",
+        //             "accept-encoding": "json",
+        //             "accept-language": "en-US,en;q=0.9,he-IL;q=0.8,he;q=0.7,ar-IL;q=0.6,ar;q=0.5",
+        //             "content-length": "122",
+        //             "referer": "https://www.mdais.org/blood-donation",
+        //             "sec-fetch-mode": "cors",
+        //             "sec-fetch-site": "same-origin",
+        //             "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"
+        //         }
+        //     };
+
+
+        //     var req = http.request(options, function (res) {
+        //         var chunks = "";
+
+        //         res.on("data", function (chunk) {
+        //             chunks += chunk.toString();
+        //         });
+
+        //         res.on("end", function () {
+        //             var obj = JSON.parse(chunks);
+
+        //             resolve(JSON.parse(obj.Result));
+        //         });
+        //     });
+
+        //     req.write(JSON.stringify({
+        //         RequestHeader:
+        //         {
+        //             Application: 101,
+        //             Module: 'BloodBank',
+        //             Function: 'GetAllDetailsDonations',
+        //             Token: ''
+        //         },
+        //         RequestData: ''
+        //     }));
+        //     req.end();
     }
 );
